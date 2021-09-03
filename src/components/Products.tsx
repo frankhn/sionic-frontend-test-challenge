08f5b7fe27875594a382f7aa37d2329b1e7fd103
frankhn <harfrank2@gmail.com>
2021-09-03 15:16:18 +0200

    add product component

---
+++ src/components/Products.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default () => {
+    return (
+        <>
+            <div>
+                categories
+            </div>
+            <div>
+                Products
+            </div>
+            <div>
+                load more
+            </div>
+        </>
+    )
+}
