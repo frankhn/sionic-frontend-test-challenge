1fead76459e48a878a365bca1f7086a2fe71348f
frankhn <harfrank2@gmail.com>
2021-09-03 15:15:40 +0200

    adds cart component

---
+++ src/components/Cart.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../components/header/NavBar'
+import Layout from '../components/Layout'
+
+export default () => {
+    return (
+        <>
+            <div>
+                Cart
+            </div>
+        </>
+    )
+}
